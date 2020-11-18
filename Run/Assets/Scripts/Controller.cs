c21691ea3a31f16e14cc9cb26c6a7831d93d6464
dinnibela <dinnibela@gmail.com>
2020-11-18 11:45:47 -0400

    codigo  zip

---
+++ Run/Assets/Scripts/Controller.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Globalization;
+
+public class Controller : MonoBehaviour
+{
+    public GameObject Jugador;
+    public Camera Camara;
+    public GameObject[] BloquePreFab;
+    public float puntero;
+    public float Generacion = 5;
+    public Text TxtJuego;
+    public bool perdiste;
+
+    private AudioSource sonido;
+    public AudioClip Golpe;
+    public AudioSource Musica;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        puntero = -7;
+        perdiste = false;
+        sonido = GetComponent<AudioSource>();
+        Musica.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Jugador != null)
+        {
+            Camara.transform.position = new Vector3(Jugador.transform.position.x, Camara.transform.position.y, Camara.transform.position.z);
+            TxtJuego.text = "Puntaje: " + Mathf.Floor(Jugador.transform.position.x);
+        }
+        else
+        {
+            if(!perdiste)
+            {
+                perdiste = true;
+                TxtJuego.text += " \n PERDISTE \n Presione R para Volver";
+                sonido.PlayOneShot(Golpe, 1f);
+                Musica.Stop();
+
+            }
+            if (perdiste)
+            {
+                if (Input.GetKeyDown("r"))
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                }
+            }
+        }
+        
+        while(Jugador!=null && puntero< Jugador.transform.position.x + Generacion)
+        {
+            int indiceBloque = Random.Range(0, BloquePreFab.Length - 1);
+            if(puntero<0)
+            {
+                indiceBloque = 5;
+
+            }
+            GameObject objetoBloque = Instantiate(BloquePreFab[indiceBloque]);
+            objetoBloque.transform.SetParent(this.transform);
+            Bloque bloque = objetoBloque.GetComponent<Bloque>();
+            objetoBloque.transform.position = new Vector2(puntero + bloque.tamano / 2,0);
+            puntero += bloque.tamano;
+            
+        }
+        
+    }
+}
---
+++ Run/Assets/Scripts/Jugador.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador : MonoBehaviour
+{
+    public int FSalto;
+    public int Velo;
+    public bool ifPiso = false;
+    private AudioSource sonido;
+    public AudioClip salto;
+    public AudioClip caida;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sonido = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space") && ifPiso)
+        {
+            sonido.PlayOneShot(salto, 1f);
+            ifPiso=false;
+            this.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, FSalto));
+
+        }
+        this.GetComponent<Rigidbody2D>().velocity=new Vector2(Velo,this.GetComponent<Rigidbody2D>().velocity.y);
+    }
+    //private void OnTriggerEnter2D(Collider2D col)
+    //{
+    //    ifPiso = true;
+    //    if(col.tag == "Obst")
+    //    {
+    //        GameObject.Destroy(this.gameObject);
+    //    }
+
+    //}
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    ifPiso = true;
+
+    //}
+    private void OnCollisionEnter2D(Collision2D c1)
+    {
+        ifPiso = true;
+        sonido.PlayOneShot(caida, 1f);
+        if (c1.collider.gameObject.tag == "Obst")
+        {
+            GameObject.Destroy(this.gameObject);
+    }
+
+}
+}
